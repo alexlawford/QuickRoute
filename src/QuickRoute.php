335027aa0ef03c0c5ad23a6a36a90dd2753f3a4f
Alexander Browne <alexanderbrowne@Alexanders-iMac.local>
2019-03-13 09:51:48 +0800

    Implementation of QuickRoute class, without matching.

--- src/QuickRoute.php
+++ src/QuickRoute.php
@@ -4,8 +4,26 @@ namespace AlexLawford\QuickRoute;
 
 class QuickRoute
 {
-  public function __invoke(string $httpMethod, string $uri, array $routes): ?Route
+  public function __invoke(string $requestMethod, string $requestUri, array $routes): ?Route
   {
+    if(count($routes) === 0){
+      return null;
+    }
 
+    [$method, $pattern, $callback] = $routes[0];
+
+    if($method === $requestMethod) {
+      $matches = $this->match($requestUri, $pattern);
+      if(count($matches) > 0) {
+        return new Route($callback, $matches);
+      }
+    }
+
+    return $this->__invoke($requestMethod, $requestUri, array_slice($routes, 1));
+  }
+
+  private function match(string $requestUri, string $pattern): array
+  {
+    return ["test"];
   }
 }
